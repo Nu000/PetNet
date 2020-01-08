3d0eb66f17b28209c62c184612ba90d0495b1523
nu <nuwanthika.mullegama@pearson.com>
2020-01-08 12:39:21 +0530

    first commit

---
+++ constants/Images.js
@@ -0,0 +1,26 @@
+const Onboarding = 'https://images.unsplash.com/photo-1544943910-4c1dc44aab44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=640&q=80';
+const Pro = 'https://images.unsplash.com/photo-1485796826113-174aa68fd81b?fit=crop&w=840&q=80';
+const Products = {
+  'Accessories': 'https://source.unsplash.com//l1MCA0VyNrk/840x840',
+};
+
+const Profile = 'https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80';
+const Avatar = 'https://images.unsplash.com/photo-1518725522904-4b3939358342?fit=crop&w=210&q=80';
+
+const Viewed = [
+  'https://images.unsplash.com/photo-1534229323250-20fdb0ce5916?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1511044568932-338cba0ad803?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=8080',
+  'https://images.unsplash.com/photo-1574388043891-87082df8d6e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80',
+  'https://images.unsplash.com/photo-1576661343141-9f7f399f72dd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1534235844982-e021907f6d39?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1516109893543-64061f89dfb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+];
+
+export default {
+  Onboarding,
+  Pro,
+  Products,
+  Profile,
+  Viewed,
+  Avatar,
+}
